4d7a4825a7663f0c4abe0d6002fcd1e7a8eebb45
Jiwoon Kim <diana0501@naver.com>
2018-08-17 08:30:08 +0000

    login now works fine but register has routing problem

--- helpers.py
+++ helpers.py
@@ -2,7 +2,6 @@ import os
 from flask import redirect, render_template, request, session
 from functools import wraps
 
-
 def error(message):
     """Render error message to user."""
 
@@ -18,19 +17,3 @@ def error(message):
 
     # render error.html with escaped message
     return render_template("error.html", message=escape(message))
-
-
-def login_required(f):
-    """
-    Decorate routes to require login.
-
-    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
-
-    based on CS50 pset7 code.
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
--- import.py
+++ import.py
@@ -15,12 +15,14 @@ db = scoped_session(sessionmaker(bind=engine))
 f= open("books.csv")
 reader = csv.reader(f)
 
-# read and insert data while skipping the first row
+# read and insert data
 count = 0
 print(f"{count}")
 for isbn, title, author, year in reader:
+  # skip the first row
   if count == 0:
     count += 1
     continue
+  # insert the next rows
   db.execute("INSERT INTO books(isbn, title, author, year) VALUES(:isbn, :title, :author, :year)", {"isbn": isbn, "title": title, "author": author, "year": year})
 db.commit()
